cb0b30b42eee57eb6c2fb71fdd39568f092713d0
Smiljana Knezev <sknezev@renderedtext.com>
2018-08-16 11:23:14 +0200

    remove sub dir

--- gs_db/deps/wormhole/lib/wormhole/defaults.ex
+++
@@ -1,9 +0,0 @@
-defmodule Wormhole.Defaults do
-  def timeout,     do: 5_000
-  def retry_count, do: 1
-  def backoff_ms,  do: 1_000
-  def crush_report,  do: false
-  def jitter,      do: :rand.uniform(250)
-  def skip_log,    do: false
-  def stacktrace,  do: false
-end
